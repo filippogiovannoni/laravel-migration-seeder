d3923bda78d3458c80a262220360f66ad931e59f
filippogiovannoni <filippogiovannonii@gmail.com>
2024-04-26 16:10:41 +0200

    add partials

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,49 +1,14 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>@yield('pageTitle', 'LaraTrains')</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-    {{-- Bootstrap --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-</head>
+@include('partials.head')
 
 <body class="antialiased">
 
-    <header>
-        <nav class="navbar navbar-expand-sm navbar-light bg-dark">
-            <div class="container">
-                <a class="navbar-brand text-white" href="#">LaraTrains</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID"
-                    aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarID">
-                    <div class="navbar-nav">
-                        <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
-
-                    </div>
-                </div>
-            </div>
-        </nav>
-    </header>
+    @include('partials.header')
 
     <main>
         @yield('content')
     </main>
 
-    <footer></footer>
+    @include('partials.footer')
 
 </body>
 
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <nav class="navbar navbar-expand-sm navbar-light bg-dark">
+        <div class="container">
+            <a class="navbar-brand text-white" href="#">LaraTrains</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID"
+                aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarID">
+                <div class="navbar-nav">
+                    <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
+
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
